2ac3d8e010740f2b8b37f874b379741501c6a3d0
Abhishek K M <mightyincognito07@gmail.com>
2026-01-27 15:27:36 +0530

    Reverted image responsiveness issues in the Backed By section

--- components/sections/BackedBy.tsx
+++ components/sections/BackedBy.tsx
@@ -18,29 +18,29 @@ export default function OurPartners() {
             repeatType: "loop",
           }}
         >
-          <Image src="/wtfweb.png" width={180} height={50} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="WTF" />
-          <Image src="/Bits.png" width={180} height={70} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="BITS Pilani" />
-          <Image src="/meity.png" width={150} height={70} className="w-[100px] xs:w-[120px] sm:w-[135px] md:w-[150px] h-auto" alt="Meity" />
-          <Image src="/Gradcap.png" width={200} height={50} className="w-[140px] xs:w-[160px] sm:w-[180px] md:w-[200px] h-auto" alt="Grad capital" />
-          <Image src="/Nucleate.png" width={240} height={40} className="w-[160px] xs:w-[180px] sm:w-[210px] md:w-[240px] h-auto" alt="Nucleate" />
-          <Image src="/ccamp.png" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="CCAMP" />
-          <Image src="/nidhi.jpg" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="DST NIDHI" />
-          <Image src="/Savant.svg" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="Savant" />
-          <Image src="/susmafia.png" width={200} height={60} className="w-[140px] xs:w-[160px] sm:w-[180px] md:w-[200px] h-auto" alt="Susmafia" />
-          <Image src="/Meractus.png" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="Meractus" />
+          <Image src="/wtfweb.png" width={180} height={50} className="w-[180px] h-[50px]" alt="WTF" />
+          <Image src="/Bits.png" width={180} height={70} className="w-[180px] h-[70px]" alt="BITS Pilani" />
+          <Image src="/meity.png" width={150} height={70} className="w-[150px] h-[70px]" alt="Meity" />
+          <Image src="/Gradcap.png" width={200} height={50} className="w-[200px] h-[50px]" alt="Grad capital" />
+          <Image src="/Nucleate.png" width={240} height={40} className="w-[240px] h-[40px]" alt="Nucleate" />
+          <Image src="/ccamp.png" width={180} height={60} className="w-[180px] h-[60px]" alt="CCAMP" />
+          <Image src="/nidhi.jpg" width={180} height={60} className="w-[180px] h-[60px]" alt="DST NIDHI" />
+          <Image src="/Savant.svg" width={180} height={60} className="w-[180px] h-[60px]" alt="Savant" />
+          <Image src="/susmafia.png" width={200} height={60} className="w-[200px] h-[60px]" alt="Susmafia" />
+          <Image src="/Meractus.png" width={180} height={60} className="w-[180px] h-[60px]" alt="Meractus" />
 
           {/*Second set*/}
 
-          <Image src="/wtfweb.png" width={180} height={50} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="WTF" />
-          <Image src="/Bits.png" width={180} height={70} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="BITS Pilani" />
-          <Image src="/meity.png" width={150} height={70} className="w-[100px] xs:w-[120px] sm:w-[135px] md:w-[150px] h-auto" alt="Meity" />
-          <Image src="/Gradcap.png" width={200} height={50} className="w-[140px] xs:w-[160px] sm:w-[180px] md:w-[200px] h-auto" alt="Grad capital" />
-          <Image src="/Nucleate.png" width={240} height={40} className="w-[160px] xs:w-[180px] sm:w-[210px] md:w-[240px] h-auto" alt="Nucleate" />
-          <Image src="/ccamp.png" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="CCAMP" />
-          <Image src="/nidhi.jpg" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="DST NIDHI" />
-          <Image src="/Savant.svg" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="Savant" />
-          <Image src="/susmafia.png" width={200} height={60} className="w-[140px] xs:w-[160px] sm:w-[180px] md:w-[200px] h-auto" alt="Susmafia" />
-          <Image src="/Meractus.png" width={180} height={60} className="w-[120px] xs:w-[140px] sm:w-[160px] md:w-[180px] h-auto" alt="Meractus" />
+          <Image src="/wtfweb.png" width={180} height={50} className="w-[180px] h-[50px]" alt="WTF" />
+          <Image src="/Bits.png" width={180} height={70} className="w-[180px] h-[70px]" alt="BITS Pilani" />
+          <Image src="/meity.png" width={150} height={70} className="w-[150px] h-[70px]" alt="Meity" />
+          <Image src="/Gradcap.png" width={200} height={50} className="w-[200px] h-[50px]" alt="Grad capital" />
+          <Image src="/Nucleate.png" width={240} height={40} className="w-[240px] h-[40px]" alt="Nucleate" />
+          <Image src="/ccamp.png" width={180} height={60} className="w-[180px] h-[60px]" alt="CCAMP" />
+          <Image src="/nidhi.jpg" width={180} height={60} className="w-[180px] h-[60px]" alt="DST NIDHI" />
+          <Image src="/Savant.svg" width={180} height={60} className="w-[180px] h-[60px]" alt="Savant" />
+          <Image src="/susmafia.png" width={200} height={60} className="w-[200px] h-[60px]" alt="Susmafia" />
+          <Image src="/Meractus.png" width={180} height={60} className="w-[180px] h-[60px]" alt="Meractus" />
         </motion.div>
       </div>
     </>
